31aea70ec427c7716d8e0c1bd70568e89b37972d
xiyao <xiyaozhang666@gmail.com>
2025-11-08 16:36:15 +0100

    update

--- joueur.py
+++ joueur.py
@@ -1,27 +1,34 @@
+from ObjetConso import ObjetsConsommables
+from objet_permanent import ObjetPermanent
+
 class Joueur:
     def __init__(self):
         # nombre de pas initial
         self.pas = 70
 
-        # Inventaire consommable (sofia)
-        self.consommable = []
+        # Inventaire consommable (Sofia)
+        self.consommables = ObjetsConsommables()
 
-        # les objets permanents
+        # les objets permanents 
         self.objets_permanents = []
 
-        # les effets des objets permanents
-        self.peut_creuser = False                # Pelle
-        self.peut_briser_cadenas_coffre = False  # Marteau
-        self.peut_ouvrir_portes = False       # Kit de crochetage
+        # effets liés aux objets permanents
+        self.peut_creuser = False
+        self.peut_briser_cadenas_coffre = False
+        self.peut_ouvrir_portes = False
 
-        self.chance_cle_pieces = 0                # Détecteur 
-        self.chance_objets = 0                     # Patte de lapin 
+        self.chance_cle_pieces = 0
+        self.chance_objets = 0
 
-    # ajouter les objets permanents
-    def ajouter_objet_permanent(self, objet):
+    # ajouter un objet permanent 
+    def ajouter_objet_permanent(self, objet: ObjetPermanent):
         self.objets_permanents.append(objet)
-        objet.appliquer(self)  
+        objet.appliquer(self)
 
+    # affichage 
     def afficher_objets_permanents(self):
+        print("Objets permanents :")
+        if not self.objets_permanents:
+            print("Aucun objet permanent.")
         for obj in self.objets_permanents:
             print(f"- {obj.nom} : {obj.description}")
